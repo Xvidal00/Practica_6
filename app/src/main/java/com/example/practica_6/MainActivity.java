d280a84d5648d2af701e3cab52dc3f673b18a9f0
Carlos <carlos.12.solrac@gmail.com>
2020-11-30 09:54:42 +0100

    s

--- app/src/main/java/com/example/practica_6/MainActivity.java
+++ app/src/main/java/com/example/practica_6/MainActivity.java
@@ -2,8 +2,12 @@ package com.example.practica_6;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
+import android.widget.ImageButton;
+import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
@@ -11,5 +15,59 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        ImageButton restaurant = findViewById(R.id.img1);
+        ImageButton transport = findViewById(R.id.img2);
+        ImageButton hotel = findViewById(R.id.img3);
+        ImageButton parking = findViewById(R.id.img4);
+        ImageButton negocis = findViewById(R.id.img5);
+        ImageButton movies = findViewById(R.id.img6);
+
+        restaurant.setOnClickListener(this);
+        transport.setOnClickListener(this);
+        hotel.setOnClickListener(this);
+        parking.setOnClickListener(this);
+        negocis.setOnClickListener(this);
+        movies.setOnClickListener(this);
+
+    }
+
+
+    @Override
+    public void onClick(View view) {
+
+        ImageView obrir = (ImageView) view;
+
+        if(obrir.getId()== R.id.img1) //RESTAURANT CAMBIAR
+        {
+            Intent RESTAURANT = new Intent(this,MainActivity.class);
+            startActivity(RESTAURANT);
+        }
+        else if(obrir.getId()== R.id.img2) //TRANSPORT CAMBIAR
+        {
+            Intent TRANSPORT = new Intent(this,MainActivity.class);
+            startActivity(TRANSPORT);
+        }
+        else if(obrir.getId()== R.id.img3) //HOTEL
+        {
+            Intent HOTEL = new Intent(this,intentHotel.class);
+            startActivity(HOTEL);
+        }
+        else if(obrir.getId()== R.id.img4) //PARKING
+        {
+            Intent PARKING = new Intent(this,intentParking.class);
+            startActivity(PARKING);
+        }
+        else if(obrir.getId()== R.id.img5) //NEGOCIS CAMBIAR
+        {
+            Intent NEGOCIS = new Intent(this,MainActivity.class);
+            startActivity(NEGOCIS);
+        }
+        else if(obrir.getId()== R.id.img6) //MOVIES CAMBIAR
+        {
+            Intent MOVIES = new Intent(this,MainActivity.class);
+            startActivity(MOVIES);
+        }
+
     }
 }
