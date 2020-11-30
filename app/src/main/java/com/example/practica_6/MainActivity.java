d112dd591d711bd7b04c58a8b6581fba7768c6e9
Xavi <xaviervidal00@hotmail.es>
2020-11-30 09:17:38 +0100

    Actualizacion

--- app/src/main/java/com/example/practica_6/MainActivity.java
+++ app/src/main/java/com/example/practica_6/MainActivity.java
@@ -3,8 +3,9 @@ package com.example.practica_6;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
